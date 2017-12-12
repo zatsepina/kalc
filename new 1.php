1fd2621847205cd24c6b76f3c20b62bc59af841d
zatsepina <32477645+zatsepina@users.noreply.github.com>
2017-12-13 00:50:08 +0700

    Update new 1.php

--- new 1.php
+++ new 1.php
@@ -1,54 +1,62 @@
-<?php
-error_reporting(-1);
-if (isset($_GET['send'])){
-    $a = (int) $_GET['a'];
-    $b = (int) $_GET['b'];
-    $c = (int) $_GET['c'];
-
-    $result = eq_roots($a, $b, $c);
-}
-function eq_roots($a, $b, $c) {
-    if ($a==0) return false;
-
-    if ($b==0) {
-        if ($c<0) {
-            $x1 = sqrt(abs($c/$a));
-            $x2 = sqrt(abs($c/$a));
-        } elseif ($c==0) {
-            $x1 = $x2 = 0;
-        } else {
-            $x1 = sqrt($c/$a).'i';
-            $x2 = -sqrt($c/$a).'i';
-        }
-    } else {
-        $d = $b*$b-4*$a*$c;
-        if ($d>0) {
-            $x1 = (-$b+sqrt($d))/2*$a;
-            $x2 = (-$b-sqrt($d))/2*$a;
-        } elseif ($d==0) {
-            $x1 = $x2 = (-$b)/2*$a;
-        } else {
-            $x1 = -$b . '+' . sqrt(abs($d)) . 'i';
-            $x2 = -$b . '-' . sqrt(abs($d)) . 'i';
-        }
-    }
-    return array($x1, $x2);
-}
-?>
-
-<p>Введите коэффициенты :</p>
-<h4>a * x<sup>2</sup> + b * x + с = 0</h4>
-<form method="GET">
-    <p>a = <input type="text" name="a" /></p>
-    <p>b = <input type="text" name="b" /></p>
-    <p>c = <input type="text" name="c" /></p>
-    <input type="submit" name="send" value="решить"/>
-</form>
-
-<? if (isset($result)): ?>
-<hr />
-<h3><?=$a ?> * x<sup>2</sup> + <?=$b ?> * x + <?=$c ?> = 0</h3>
-<p>Корни уравнения :</p>
-<p>X<sub>1</sub> = <?=$result[0] ?></p>
-<p>X<sub>2</sub> = <?=$result[1] ?></p>
-<? endif ?>
+<html>
+	<head>
+		<title>Решение квадратного уравнения</title>
+	</head>
+	<body>
+		<?php
+			if (isset($_GET['a'])) {
+				$a = $_GET['a'];
+			} else {
+				$a = '';
+			}
+			if (isset($_GET['b'])) {
+				$b = $_GET['b'];
+			} else {
+				$b = '';
+			}
+			if (isset($_GET['c'])) {
+				$c = $_GET['c'];
+			} else {
+				$c = '';
+			}
+		?>
+		
+		<h1>Решение квадратного уравнения</h1>
+		<h3>Введите коэффициенты :</h3>
+		<h3>a * x<sup>2</sup> + b * x + с = 0</h3>
+		
+		<form method="GET">
+			<p>a = <input type="text" name="a" value="<?= htmlspecialchars($a); ?>"></p>
+			<p>b = <input type="text" name="b" value="<?= htmlspecialchars($b); ?>"></p>
+			<p>c = <input type="text" name="c" value="<?= htmlspecialchars($c); ?>"></p>
+			<input type="submit" value="Решить">
+		</form>
+		
+		<hr/>
+		
+		<?php
+			$a = str_replace(',', '.', $a);
+			$b = str_replace(',', '.', $b);
+			$c = str_replace(',', '.', $c);
+			
+			if (is_numeric($a) && is_numeric($b) && is_numeric($c)) {
+				$D = pow($b, 2) - 4*$a*$c;
+					if ($D > 0) {
+						$x1 = ((-1)*$b+sqrt($D))/(2*$a);
+						$x2 = ((-1)*$b-sqrt($D))/(2*$a);
+						$x1 = number_format($x1, 2, ',', '');
+						$x2 = number_format($x2, 2, ',', '');
+						echo "Квадратное уравнение:<br/> <b> $a x<sup>2</sup> + $b x + $c = 0</b> <br/> имеет два решения <br/> x<sub>1</sub> = $x1; <br/> x<sub>2</sub> = $x2;";
+					} elseif ($D == 0) {
+						$x=((-1)*$b)/(2*$a);
+						$x = number_format($x, 2, ',', '');
+						echo "Квадратное уравнение:<br/> <b> $a x<sup>2</sup> + $b x + $c = 0</b> <br/> имеет  одно решение <br/> x = $x;";
+					} else {
+						echo "Квадратное уравнение:<br/> <b> $a x<sup>2</sup> + $b x + $c = 0</b> <br/> не имеет решений";
+					}
+			} else if ($a != '' || $b != ''|| $c != ''){
+				echo 'Переменные не могут являться не числовыми значениями';
+			}
+		?>
+	</body>
+</html>
